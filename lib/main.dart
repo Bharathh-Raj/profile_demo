5a3d22c7b4d51106dd917cc79b16450c85e102f3
kokki kumaru <maheshwaran8232.com>
2019-12-26 18:11:41 +0530

    prof desc finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-
+import './pages/profile_description_page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return MaterialApp(home: Profile_description());
   }
 }
