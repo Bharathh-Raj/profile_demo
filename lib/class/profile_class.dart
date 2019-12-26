6597b4ec092309d8e0302214388eda910806ae28
Bharathhraj <41340472+Bharathhraj@users.noreply.github.com>
2019-12-26 13:56:34 +0530

    App skeleton

---
+++ lib/class/profile_class.dart
@@ -0,0 +1,21 @@
+class Profile{
+  String name;
+  String desc;
+  String numb;
+  dynamic propic;
+  int age;
+  bool gender;
+  String city;
+
+
+  Profile({
+    this.name,
+    this.desc,
+    this.numb,
+    this.propic,
+    this.age,
+    this.gender,
+    this.city
+    });
+
+}
